6fbf022202cc1c3be718f35e11e3eef1276f1ae7
Filipe Santos <filipesantos.dev@gmail.com>
2023-12-05 17:03:52 +0000

    server

--- lua/awman/remap.lua
+++ lua/awman/remap.lua
@@ -42,9 +42,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
@@ -83,5 +80,3 @@ vim.keymap.set("n", "<M-k>", ":m-2<CR>==");
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>");
 vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>");
 vim.keymap.set("n", "<leader>gp", "<cmd>:Git push -u origin<CR>");
-
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
