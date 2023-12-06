1e856ff7e718953b81da5f782f3719919862a35e
Filipe Santos <filipesantos.dev@gmail.com>
2023-12-06 15:45:39 +0000

    Remaped the git commands

--- lua/awman/remap.lua
+++ lua/awman/remap.lua
@@ -66,7 +66,7 @@ vim.keymap.set("n","<leader>bm",function() require("bookmarks").bookmark_toggle(
 vim.keymap.set("n","<leader>bi",function() require("bookmarks").bookmark_toggle() end, {--[[ add or remove bookmark at current line]]}) 
 vim.keymap.set("n","<leader>bme",function() require("bookmarks").bookmark_ann() end, {--[[add or edit mark annotation at current line]]}) 
 vim.keymap.set("n","<leader>be",function() require("bookmarks").bookmark_ann() end, {--[[add or edit mark annotation at current line]]}) 
-vim.keymap.set("n","<leader>bc",function() require("bookmarks").bookmark_clean() end, {--[[clean all marks in local buffer]]}) -- 
+vim.keymap.set("n","<leader>bc",function() require("bookmarks").bookmark_clean() end, {--[[clean all marks in local buffer]]}) 
 vim.keymap.set("n","<leader>bn",function() require("bookmarks").bookmark_next() end, {--[[jump to next mark in local buffer]]}) 
 vim.keymap.set("n","<leader>bp",function() require("bookmarks").bookmark_prev() end, {--[[jump to previous mark in local buffer]]}) 
 vim.keymap.set("n","<leader>bl",function() require("bookmarks").bookmark_list() end, {--[[show marked file list in quickfix window]]}) 
@@ -79,6 +79,12 @@ vim.keymap.set("v", "<Tab>", ">gv");
 vim.keymap.set("n", "<M-j>", ":m+<CR>==");
 vim.keymap.set("n", "<M-k>", ":m-2<CR>==");
 
-vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>");
-vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>");
-vim.keymap.set("n", "<leader>gp", "<cmd>:Git push -u origin<CR>");
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {--[[Git commit]]});
+vim.keymap.set("n","<leader>gd", "<cmd>:Gdiff<CR>", {--[[Git difference]]});
+vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>", {--[[Use left]]});
+vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>", {--[[Use left]]});
+vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>", {--[[Use righ]]});
+vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>", {--[[Use righ]]});
+vim.keymap.set("n","<leader>gc", "<cmd>:Git commit<CR>", {--[[Git commit]]}); 
+vim.keymap.set("n", "<leader>gp", "<cmd>:Git push -u origin<CR>", {--[[Git commit]]});
+
