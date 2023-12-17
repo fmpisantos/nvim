c36e9638f5977c87743eba6bf97d30ab7ac737d4
Filipe Santos <filipesantosdev@gmail.com>
2023-12-17 21:38:02 +0000

    Fix: fixed lsp config

--- lua/awman/plugins/lsp.lua
+++ lua/awman/plugins/lsp.lua
@@ -1,83 +1,156 @@
 return {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v2.x',
-	dependencies = {
-		-- LSP Support
-		{'neovim/nvim-lspconfig'},             -- Required
-		{'williamboman/mason.nvim'},           -- Optional
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	},
-	config = function()
-		local lsp = require("lsp-zero")
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim', opts = {} },
 
-		lsp.preset("recommended")
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+			{
+				-- Autocompletion
+				'hrsh7th/nvim-cmp',
+				dependencies = {
+					-- Snippet Engine & its associated nvim-cmp source
+					'L3MON4D3/LuaSnip',
+					'saadparwaiz1/cmp_luasnip',
 
-		-- Fix Undefined global 'vim'
-		lsp.nvim_workspace()
-		local cmp = require('cmp')
-		local cmp_select = {behavior = cmp.SelectBehavior.Select}
-		local cmp_mappings = lsp.defaults.cmp_mappings({
-			['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-			['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-			['<C-y>'] = cmp.mapping.confirm({ select = true }),
-			["<C-Space>"] = cmp.mapping.complete(),
-		})
+					-- Adds LSP completion capabilities
+					'hrsh7th/cmp-nvim-lsp',
+					'hrsh7th/cmp-path',
 
-		cmp_mappings['<Tab>'] = nil
-		cmp_mappings['<S-Tab>'] = nil
+					-- Adds a number of user-friendly snippets
+					'rafamadriz/friendly-snippets',
+				},
+			},
+			{ 'folke/which-key.nvim', opts = {} }
+		},
+		config = function()
+			local on_attach = function(_, bufnr)
+				local nmap = function(keys, func, desc)
+					if desc then
+						desc = 'LSP: ' .. desc
+					end
 
-		lsp.setup_nvim_cmp({
-			mapping = cmp_mappings
-		})
+					vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+				end
 
-		lsp.set_preferences({
-			suggest_lsp_servers = false,
-			sign_icons = {
-				error = 'E',
-				warn = 'W',
-				hint = 'H',
-				info = 'I'
+				nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+				nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+				nmap('<leader>vca', vim.lsp.buf.code_action, '[V]iew [C]ode [A]ction')
+				nmap('<leader>vd', vim.diagnostic.open_float,'[V]iew [D]ialog');
+				nmap('<leader>vs', vim.lsp.buf.signature_help,'[V]iew [S]igniture');
+
+				nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+				nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+				nmap('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+				nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+				nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+				nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+				nmap('<M-\\>', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+				nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+				nmap('<leader>ps', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[P]roject [S]ymbols')
+
+				nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+				-- Create a command `:Format` local to the LSP buffer
+				vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+					vim.lsp.buf.format()
+					end, { desc = 'Format current buffer with LSP' })
+			end
+
+			require('which-key').register {
+				['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+				['<leader>v'] = { name = '[V]iew', _ = 'which_key_ignore' },
+				['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+				['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
+				['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+				['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+				['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+				['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+				['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+				['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 			}
-		})
 
-		lsp.on_attach(function(_, bufnr)
-			local opts = {buffer = bufnr, remap = false}
+			require('which-key').register({
+				['<leader>'] = { name = 'VISUAL <leader>' },
+				['<leader>h'] = { 'Git [H]unk' },
+				}, { mode = 'v' })
 
-            local nmap = function(keys, func, desc)
-                if desc then
-                    desc = 'LSP: ' .. desc
-                end
+			require('mason').setup()
+			require('mason-lspconfig').setup()
 
-                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-            end
+			local servers = {
+				rust_analyzer = {},
+				jdtls = {},
+				lua_ls = {
+					Lua = {
+						workspace = { checkThirdParty = false },
+						telemetry = { enable = false },
+					},
+				},
+			}
 
-            nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-            nmap('<leader>vr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-            nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-            nmap('<M-\\>', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>\\', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>ps', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[P]roject [S]ymbols')
-			opts["desc"] = "[V]iew [D]ialog";
-			vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts);
-			opts["desc"] = "[V]iew [C]ode [A]ctions";
-			vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts);
-			opts["desc"] = "[R]e-[N]ame";
-			vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts);
-			opts["desc"] = "[V]iew [S]igniture";
-			vim.keymap.set("i", "<leader>vs", function() vim.lsp.buf.signature_help() end, opts);
-		end)
+			-- Setup neovim lua configuration
+			require('neodev').setup()
 
-		lsp.setup()
+			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-		vim.diagnostic.config({
-			virtual_text = true
-		})
+			-- Ensure the servers above are installed
+			local mason_lspconfig = require 'mason-lspconfig'
 
-	end
+			mason_lspconfig.setup {
+				ensure_installed = vim.tbl_keys(servers),
+			}
+
+			mason_lspconfig.setup_handlers {
+				function(server_name)
+					require('lspconfig')[server_name].setup {
+						capabilities = capabilities,
+						on_attach = on_attach,
+						settings = servers[server_name],
+						filetypes = (servers[server_name] or {}).filetypes,
+					}
+				end,
+			}
+
+			-- [[ Configure nvim-cmp ]]
+			-- See `:help cmp`
+			local cmp = require 'cmp'
+			local luasnip = require 'luasnip'
+			require('luasnip.loaders.from_vscode').lazy_load()
+			luasnip.config.setup {}
+
+			cmp.setup {
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = {
+					completeopt = 'menu,menuone,noinsert',
+				},
+				mapping = cmp.mapping.preset.insert {
+					['<C-n>'] = cmp.mapping.select_next_item(),
+					['<C-p>'] = cmp.mapping.select_prev_item(),
+					['<C-y>'] = cmp.mapping.confirm({ select = true }),
+					['<C-Space>'] = cmp.mapping.complete {},
+					['<Tab>'] = nil,
+					['<S-Tab>'] = nil,
+				},
+				sources = {
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+					{ name = 'path' },
+				},
+			}
+		end
+	}
 }
