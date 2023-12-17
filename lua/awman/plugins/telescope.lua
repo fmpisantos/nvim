ebb487f5b467bbd68566a2ccc24d4ac13d7fb26f
Filipe Santos <filipesantosdev@gmail.com>
2023-12-17 17:29:24 +0000

    Update: Updated telescope version

--- lua/awman/plugins/telescope.lua
+++ lua/awman/plugins/telescope.lua
@@ -1,5 +1,7 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.x',
+	'nvim-telescope/telescope.nvim', 
+	tag = '0.1.5',
+	--  tag = '0.1.x'
 	dependencies = {
 		'nvim-lua/plenary.nvim'
 	},
