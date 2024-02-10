746a0edfaaeb0db9ee85f84dd53a834f4dfe0ec6
Filipe Santos <filipesantosdev@gmail.com>
2024-02-10 17:31:31 +0000

    feat: if in visual mode telescope searches now use the selected word(s) by default

--- lua/awman/plugins/telescope.lua
+++ lua/awman/plugins/telescope.lua
@@ -1,16 +1,51 @@
+function vim.getVisualSelection()
+    vim.cmd('noau normal! "vy"')
+    local text = vim.fn.getreg('v')
+    vim.fn.setreg('v', {})
+
+    text = string.gsub(text, "\n", "")
+    if #text > 0 then
+        return text
+    else
+        return ''
+    end
+end
+
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	--  tag = '0.1.x'
-	dependencies = {
-		'nvim-lua/plenary.nvim'
-	},
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "[P]roject [F]ile" });
-		vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "[P]roject [G]rep" });
-		vim.keymap.set('n', '<leader>dg',function() builtin.live_grep({search_dirs={vim.fn.expand("%:p")}}) end,{ desc = "[D]ocument [G]rep" });
-		vim.keymap.set('n', '<leader>fg',function() builtin.live_grep({search_dirs={vim.fn.expand("%:h")}}) end,{ desc = "[F]older [G]rep" });
-		vim.keymap.set('n', '<leader><leader>', "<cmd>:Telescope keymaps<CR>", { desc = "Grep over keymaps" });
-	end
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    --  tag = '0.1.x'
+    dependencies = {
+        'nvim-lua/plenary.nvim'
+    },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "[P]roject [F]ile" });
+        vim.keymap.set('v', '<leader>pf', function()
+            local text = vim.getVisualSelection()
+            builtin.find_files({ default_text = text })
+        end, { desc = "[P]roject [F]ile" });
+
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "[P]roject [G]rep" });
+        vim.keymap.set('v', '<leader>pg', function()
+            local text = vim.getVisualSelection()
+            builtin.live_grep({ default_text = text })
+        end, { desc = "[P]roject [G]rep" });
+
+        vim.keymap.set('n', '<leader>dg', function() builtin.live_grep({ search_dirs = { vim.fn.expand("%:p") } }) end,
+            { desc = "[D]ocument [G]rep" });
+        vim.keymap.set('v', '<leader>dg', function()
+            local text = vim.getVisualSelection()
+            builtin.live_grep({ search_dirs = { vim.fn.expand("%:p") }, default_text = text })
+        end, { desc = "[D]ocument [G]rep" });
+
+        vim.keymap.set('n', '<leader>fg', function() builtin.live_grep({ search_dirs = { vim.fn.expand("%:h") } }) end,
+            { desc = "[F]older [G]rep" });
+        vim.keymap.set('v', '<leader>fg', function()
+            local text = vim.getVisualSelection()
+            builtin.live_grep({ search_dirs = { vim.fn.expand("%:h") }, default_text = text })
+        end, { desc = "[F]older [G]rep" });
+
+        vim.keymap.set('n', '<leader><leader>', "<cmd>:Telescope keymaps<CR>", { desc = "Grep over keymaps" });
+    end
 }
