91e151450f0ee841d6f927ac692e8f6d0b4e966d
Filipe Santos <filipesantosdev@gmail.com>
2024-02-10 17:04:38 +0000

    feat: <leader>fg now greps at the current open directory (current file)

--- lua/awman/plugins/telescope.lua
+++ lua/awman/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "[P]roject [F]ile" });
 		vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "[P]roject [G]rep" });
 		vim.keymap.set('n', '<leader>dg',function() builtin.live_grep({search_dirs={vim.fn.expand("%:p")}}) end,{ desc = "[D]ocument [G]rep" });
+		vim.keymap.set('n', '<leader>fg',function() builtin.live_grep({search_dirs={vim.fn.expand("%:h")}}) end,{ desc = "[F]older [G]rep" });
 		vim.keymap.set('n', '<leader><leader>', "<cmd>:Telescope keymaps<CR>", { desc = "Grep over keymaps" });
 	end
 }
