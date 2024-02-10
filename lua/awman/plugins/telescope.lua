496fa7565fe09d86a55f245834df5d484e933304
Filipe Santos <filipesantosdev@gmail.com>
2024-02-10 16:57:36 +0000

    fix: <leader>dg now only greps over current file

--- lua/awman/plugins/telescope.lua
+++ lua/awman/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "[P]roject [F]ile" });
 		vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "[P]roject [G]rep" });
-		vim.keymap.set('n', '<leader>dg',function() builtin.live_grep({grep_open_files = true}) end,{ desc = "[D]ocument [G]rep" });
+		vim.keymap.set('n', '<leader>dg',function() builtin.live_grep({search_dirs={vim.fn.expand("%:p")}}) end,{ desc = "[D]ocument [G]rep" });
 		vim.keymap.set('n', '<leader><leader>', "<cmd>:Telescope keymaps<CR>", { desc = "Grep over keymaps" });
 	end
 }
