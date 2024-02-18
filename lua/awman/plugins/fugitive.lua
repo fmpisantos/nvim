9af4105ced3d0497437aec27ec6acdfb7992bb4b
Filipe Santos <filipesantosdev@gmail.com>
2024-02-18 20:59:35 +0000

    feat: keymap adjustments, removed dap

--- lua/awman/plugins/fugitive.lua
+++ lua/awman/plugins/fugitive.lua
@@ -1,6 +1,14 @@
 return {
-	'tpope/vim-fugitive',
-	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tart"});
-	end
+    'tpope/vim-fugitive',
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tart" });
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git commit" });
+        vim.keymap.set("n", "<leader>gd", "<cmd>:Gdiff<CR>", { desc = "Git difference" });
+        vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>", { desc = "Use left" });
+        vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>", { desc = "Use left" });
+        vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>", { desc = "Use righ" });
+        vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>", { desc = "Use righ" });
+        vim.keymap.set("n", "<leader>gc", "<cmd>:Git commit<CR>", { desc = "Git commit" });
+        vim.keymap.set("n", "<leader>gp", "<cmd>:Git push -u origin<CR>", { desc = "Git commit" });
+    end
 }
--- lua/awman/remap.lua
+++ lua/awman/remap.lua
@@ -10,28 +10,12 @@ vim.keymap.set("n", "<leader>k", "<cmd>cnext<CR>zz", { desc = "Goto prev error"}
 vim.keymap.set("n", "<F3>", "<cmd>cnext<CR>zz", { desc = "Goto prev error"})
 vim.keymap.set("n", "<leader>j", "<cmd>cprev<CR>zz", { desc = "Goto next error"})
 vim.keymap.set("n", "<S-F3>", "<cmd>cprev<CR>zz", { desc = "Goto next error"})
-vim.keymap.set("n","<leader>bm",function() require("bookmarks").bookmark_toggle() end, { desc = "add or remove bookmark at current line"});
-vim.keymap.set("n","<leader>bi",function() require("bookmarks").bookmark_toggle() end, { desc = "add or remove bookmark at current line"}) ;
-vim.keymap.set("n","<leader>bme",function() require("bookmarks").bookmark_ann() end, { desc = "add or edit mark annotation at current line"}) ;
-vim.keymap.set("n","<leader>be",function() require("bookmarks").bookmark_ann() end, { desc = "add or edit mark annotation at current line"}) ;
-vim.keymap.set("n","<leader>bc",function() require("bookmarks").bookmark_clean() end, { desc = "clean all marks in local buffer"}) ;
-vim.keymap.set("n","<leader>bn",function() require("bookmarks").bookmark_next() end, { desc = "jump to next mark in local buffer"}) ;
-vim.keymap.set("n","<leader>bp",function() require("bookmarks").bookmark_prev() end, { desc = "jump to previous mark in local buffer"}) ;
-vim.keymap.set("n","<leader>bl",function() require("bookmarks").bookmark_list() end, { desc = "show marked file list in quickfix window"}) ;
 vim.keymap.set("n", "<S-Tab>", "<<hhhh", {desc = "Remove tab"});
 vim.keymap.set("n", "<leader><Tab>", ">>llll", {desc = "Add tab"});
 vim.keymap.set("v", "<S-Tab>", "<gv", {desc = "Remove tab for selected lines"});
 vim.keymap.set("v", "<Tab>", ">gv", {desc = "Add tab to selected lines"});
 vim.keymap.set("n", "<M-j>", ":m+<CR>==", {desc = "Switch present line with line above"});
 vim.keymap.set("n", "<M-k>", ":m-2<CR>==", {desc = "Switch present line with line bellow"});
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git commit"});
-vim.keymap.set("n","<leader>gd", "<cmd>:Gdiff<CR>", { desc = "Git difference"});
-vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>", { desc = "Use left"});
-vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>", { desc = "Use left"});
-vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>", { desc = "Use righ"});
-vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>", { desc = "Use righ"});
-vim.keymap.set("n","<leader>gc", "<cmd>:Git commit<CR>", { desc = "Git commit"});
-vim.keymap.set("n", "<leader>gp", "<cmd>:Git push -u origin<CR>", { desc = "Git commit"});
 --vim.keymap.set("sb", "a", "adds file to stage or unstage list", {--[[Adds file to stage or unstage list]]});
 --vim.keymap.set("sb", "X", "resets changes to file", {--[[Resets changes to file]]});
 
