a1c1399c40a669066171b9d63e6f3b272f6e51fa
Filipe <filipesantosdev@gmail.com>
2024-04-13 00:42:12 +0100

    feat: Initial dap config

--- lua/awman/plugins/dap.lua
+++ lua/awman/plugins/dap.lua
@@ -2,6 +2,45 @@ return {
     "mfussenegger/nvim-dap",
     requires = { { "williamboman/mason.nvim" } },
     config = function()
+        local dap = require('dap')
+
+        -- Java debugger configuration
+        dap.adapters.java = {
+            type = 'executable',
+            command = 'java',
+            args = { '-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005' },
+            env = {
+                JAVA_HOME = os.getenv('JAVA_HOME')
+            },
+            name = "Java"
+        }
+
+        dap.configurations.java = {
+          {
+            type = 'java',
+            request = 'attach',
+            name = "Debug (Attach) - Remote",
+            hostName = "127.0.0.1",
+            port = 5005,
+          },
+        }
+        --
+        -- dap.configurations.java = {
+        --     {
+        --         type = 'java',
+        --         name = 'Debug (Launch)',
+        --         request = 'launch',
+        --         cwd = '${workspaceFolder}',
+        --         startup = "${file}",
+        --         stopOnEntry = true,
+        --         classpath = '${workspaceFolder}/bin',
+        --         options = {
+        --             classpath = '${workspaceFolder}/bin'
+        --         }
+        --     }
+        -- }
+
+        -- Key mappings for debugging
         vim.api.nvim_set_keymap('n', '<F5>', ":lua require'dap'.continue()<CR>", { noremap = true })
         vim.api.nvim_set_keymap('n', '<Shift-F5>', ":lua require'dap'.stop()<CR>", { noremap = true })
         vim.api.nvim_set_keymap('n', '<F9>', ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true })
