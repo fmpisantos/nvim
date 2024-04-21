dc3ac0f030b4a97e81fb63695921d21d81f8695b
Filipe <filipesantosdev@gmail.com>
2024-04-21 20:42:25 +0100

    feat: Dap now compiles maven before starting

--- lua/awman/plugins/dap.lua
+++ lua/awman/plugins/dap.lua
@@ -13,16 +13,41 @@ return {
                 port = 5005,
             },
         }
+
+
+        local function compile_mvn()
+            local handle = io.popen('mvn compile 2>&1')
+            if handle then
+                local result = handle:read('*a')
+                handle:close()
+                if result then
+                    print('Maven compilation error:\n' .. result)
+                else
+                    vim.api.nvim_err_writeln('Failed to read Maven compilation output')
+                end
+            else
+                vim.api.nvim_err_writeln('Failed to execute mvn compile')
+            end
+        end
+
         local jdtls = require("jdtls");
-        vim.keymap.set('n', '<leader>dr', function() Dap.run_last() end, { noremap = true, desc = "Debug run_last" })
-        vim.keymap.set('n', "<leader>dc", function() jdtls.test_class() end, { desc = "[D]ebug [C]lass" }) 
-        vim.keymap.set('n', '<leader>dm', function() require('jdtls').test_nearest_method() end, { desc = '[D]ebug [M]ethod' })
+        vim.keymap.set('n', '<leader>dr', function() compile_mvn(); Dap.run_last() end,
+            { noremap = true, desc = "Debug run_last" })
+        vim.keymap.set('n', "<leader>dc", function() compile_mvn(); jdtls.test_class() end, { desc = "[D]ebug [C]lass" })
+        vim.keymap.set('n', '<leader>dm', function() compile_mvn(); require('jdtls').test_nearest_method() end,
+            { desc = '[D]ebug [M]ethod' })
         vim.keymap.set('n', '<F5>', function() Dap.continue() end, { noremap = true, desc = "Degub Continue" })
         vim.keymap.set('n', '<S-F5>', function() Dap.terminate() end, { noremap = true, desc = "Debug Stop" })
-        vim.keymap.set('n', '<C-S-F5>', function() Dap.terminate();Dap.run_last(); end, { noremap = true, desc = "Debug Stop" })
-        vim.keymap.set('n', '<F9>', function() Dap.toggle_breakpoint() end, { noremap = true, desc = "Debug Toggle Breakpoint" })
-        vim.keymap.set('n', '<F10>', function() Dap.step_over() end, { noremap = true, desc = "Debug Step Over" })
-        vim.keymap.set('n', '<S-F11>', function() Dap.step_out() end, { noremap = true, desc = "Debug Step Out" })
-        vim.keymap.set('n', '<F11>', function() Dap.step_into() end, { noremap = true, desc = "Debug Step Into" })
+        vim.keymap.set('n', '<C-S-F5>', function()
+            Dap.terminate(); Dap.run_last();
+        end, { noremap = true, desc = "Debug Stop" })
+        vim.keymap.set('n', '<F9>', function() Dap.toggle_breakpoint() end,
+            { noremap = true, desc = "Debug Toggle Breakpoint" })
+        vim.keymap.set('n', '<F10>', function() Dap.step_over() end,
+            { noremap = true, desc = "Debug Step Over" })
+        vim.keymap.set('n', '<S-F11>', function() Dap.step_out() end,
+            { noremap = true, desc = "Debug Step Out" })
+        vim.keymap.set('n', '<F11>', function() Dap.step_into() end,
+            { noremap = true, desc = "Debug Step Into" })
     end,
 }
