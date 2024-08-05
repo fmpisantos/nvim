fb598597144c19ec9127a2c7446ec6e11e34cfdb
Filipe Santos <filipesantosdev@gmail.com>
2024-08-05 15:01:05 +0100

    feat: updated which-key configuration

--- lua/awman/plugins/lsp.lua
+++ lua/awman/plugins/lsp.lua
@@ -27,7 +27,13 @@ return {
                     'hrsh7th/cmp-path',
                 }
             },
-            { 'folke/which-key.nvim', opts = {} }
+            {
+                'folke/which-key.nvim',
+                opts = {
+                    show_help = false,
+                    notify = true,
+                }
+            }
         },
         opts = {
         },
@@ -111,22 +117,24 @@ return {
                 end, { desc = 'Format all project files with LSP' })
                 nmap('<leader>pF', ':FormatAll<CR>', '[P]roject [F]ormat')
             end
-            require('which-key').register {
-                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-                ['<leader>v'] = { name = '[V]iew', _ = 'which_key_ignore' },
-                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-                ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
-                ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-                ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-                ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-            }
-            require('which-key').register({
-                ['<leader>'] = { name = 'VISUAL <leader>' },
-                ['<leader>h'] = { 'Git [H]unk' },
+
+            require('which-key').add({
+                { '<leader>c', 'which_key_ignore', desc = '[C]ode' },
+                { '<leader>v', 'which_key_ignore', desc = '[V]iew' },
+                { '<leader>d', 'which_key_ignore', desc = '[D]ocument' },
+                { '<leader>p', 'which_key_ignore', desc = '[P]roject' },
+                { '<leader>g', 'which_key_ignore', desc = '[G]it' },
+                { '<leader>h', 'which_key_ignore', desc = 'Git [H]unk' },
+                { '<leader>r', 'which_key_ignore', desc = '[R]ename' },
+                { '<leader>s', 'which_key_ignore', desc = '[S]earch' },
+                { '<leader>t', 'which_key_ignore', desc = '[T]oggle' },
+                { '<leader>w', 'which_key_ignore', desc = '[W]orkspace' },
+            });
+            require('which-key').add({
+                { '<leader>',  'which_key_ignore', desc = 'VISUAL <leader>' },
+                { '<leader>h', 'which_key_ignore', desc = 'Git [H]unk' }
             }, { mode = 'v' })
+
             require('mason').setup()
             require('mason-lspconfig').setup()
             local servers = {
