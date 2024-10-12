a4efabdd0e1e8f14af3254e58f017d340ec1a544
Filipe Santos <filipesantosdev@gmail.com>
2024-10-12 13:04:47 +0100

    feat: changed theme

--- lua/awman/colorscheme.lua
+++ lua/awman/colorscheme.lua
@@ -4,7 +4,7 @@ vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
 -- vim.cmd.highlight("LineNr", { guifg = "#4b5263" })
 
 -- vim.cmd.colorscheme 'catppuccin-frappe'
--- vim.cmd.colorscheme 'catppuccin-macchiato'
+vim.cmd.colorscheme 'catppuccin-macchiato'
 -- vim.cmd.colorscheme 'catppuccin-mocha'
 
 -- vim.o.background = "dark"
@@ -14,7 +14,7 @@ vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
 -- vim.cmd.colorscheme("tokyonight-moon")
 -- vim.cmd.colorscheme("tokyonight-night")
 
-vim.cmd.colorscheme("rose-pine-main")
+-- vim.cmd.colorscheme("rose-pine-main")
 -- vim.cmd.colorscheme("rose-pine-moon")
 
 -- vim.cmd.colorscheme("monochrome")
