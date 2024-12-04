99a18c4510b7acb1d9998585554d0f1056967e14
Filipe Miguel Pinho Santos <fm.santos@sihot.com>
2024-12-04 14:50:42 +0000

    feat: Added linecoloring after the colorscheme loaded

--- lua/awman/colorscheme.lua
+++ lua/awman/colorscheme.lua
@@ -1,6 +1,12 @@
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white' })
--- vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'orange' })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white' })
+        -- vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'orange' })
+        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#ffffff' })
+        vim.api.nvim_set_hl(0, 'LineNr', { fg = '#4b5263' })
+    end,
+})
 -- vim.cmd.highlight("LineNr", { guifg = "#4b5263" })
 
 -- vim.cmd.colorscheme 'catppuccin-frappe'
