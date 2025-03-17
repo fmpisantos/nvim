24fe4a998437ca7d92e351e1118fe793419c2428
Filipe Miguel Pinho Santos <fm.santos@sihot.com>
2025-03-17 16:01:36 +0000

    Commented personal plugins

--- lua/awman/myPlugins/init.lua
+++ lua/awman/myPlugins/init.lua
@@ -1,2 +1,2 @@
-require("awman.myPlugins.floatingDiffView")
-require("awman.myPlugins.floatingTerminal")
+-- require("awman.myPlugins.floatingDiffView")
+-- require("awman.myPlugins.floatingTerminal")
