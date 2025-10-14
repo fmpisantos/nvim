b78116da3c3df87b5e6258ea11230fa27b7707d7
Filipe Miguel Pinho Santos <fm.santos@sihot.com>
2025-10-14 08:26:12 +0100

    oil: folder information by pressing i

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,28 +1,44 @@
 return {
     src = "stevearc/oil.nvim",
     setup = function()
-        require('oil').setup {
+        local oil = require("oil")
+        local actions = require("oil.actions")
+
+        local show_details = false
+
+        local function toggle_columns()
+            show_details = not show_details
+            if show_details then
+                oil.set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+                oil.set_columns({ "icon" })
+            end
+            actions.refresh.callback()
+        end
+
+        oil.setup({
             keymaps = {
                 ["<C-h>"] = false,
                 ["<Tab>"] = "actions.preview",
                 ["<C-v>"] = "actions.select_vsplit",
+                -- Use `i` to toggle detailed columns like netrw
+                ["i"] = toggle_columns,
             },
             view_options = {
-                -- Show files and directories that start with "."
                 show_hidden = true,
-                -- This function defines what is considered a "hidden" file
                 is_hidden_file = function(name, _)
                     return vim.startswith(name, ".")
                 end,
-                -- This function defines what will never be shown, even when `show_hidden` is set
                 is_always_hidden = function(_, _)
                     return false
                 end,
             },
             skip_confirm_for_simple_edits = true,
-        }
-        vim.keymap.set("n", "-", require('oil').open, { noremap = true, silent = true, desc = "Open file" });
-        vim.keymap.set("n", "<leader>pv", require('oil').open,
-            { noremap = true, silent = true, desc = "Open parent directory" });
-    end
+            columns = { "icon" }, -- start minimal
+        })
+
+        vim.keymap.set("n", "-", oil.open, { noremap = true, silent = true, desc = "Open file" })
+        vim.keymap.set("n", "<leader>pv", oil.open,
+            { noremap = true, silent = true, desc = "Open parent directory" })
+    end,
 }
