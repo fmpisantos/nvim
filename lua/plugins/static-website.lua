719e9883f274e15acb5cebe9c2b9db802b6f341f
Filipe Santos <filipesantosdev@gmail.com>
2026-02-01 18:34:57 +0000

    feat: Adjusted static-websites.lua

--- lua/plugins/static-website.lua
+++ lua/plugins/static-website.lua
@@ -4,8 +4,8 @@ return {
         local r = require('repo-specific')
         r.setup {
             match = 'fmpisantos/static-website',
-            on_match = function(bufnr, _)
-                vim.api.nvim_buf_create_user_command(bufnr, 'DeployLambda', function(_)
+            on_match = function(_, _)
+                vim.api.nvim_create_user_command('DeployLambda', function()
                     local path = vim.api.nvim_buf_get_name(0)
                     if path:match('^oil://') then
                         path = path:sub(7)
@@ -17,7 +17,7 @@ return {
                     else
                         print('Not inside a lambda folder')
                     end
-                end, { desc = 'Deploy current lambda function' });
+                end, { desc = 'Deploy current lambda function' })
             end
         }
     end
